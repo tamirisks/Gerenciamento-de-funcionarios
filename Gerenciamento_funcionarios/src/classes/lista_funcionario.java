15d7f3849eb9ff5b2eb0643604fe02c8ab9383ea
Tamiris <tamiriskdesouza@gmail.com>
2026-01-17 13:50:39 -0300

    Classe da lista para a tabela

---
+++ Gerenciamento_funcionarios/src/classes/lista_funcionario.java
@@ -0,0 +1,20 @@
+
+package classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class lista_funcionario {
+ 
+    private static List<cadastro_funcionario> DadosFuncionario = new ArrayList<>();
+
+    public static List<cadastro_funcionario> getDadosFuncionario() {
+        return DadosFuncionario;
+    }
+
+    public static void setDadosPaciente(cadastro_funcionario funcionario) {
+        DadosFuncionario.add(funcionario);
+    }    
+}
+    
+
