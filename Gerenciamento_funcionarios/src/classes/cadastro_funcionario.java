74659416a0c4e86171be4187c554d0f80799cae4
Tamiris <tamiriskdesouza@gmail.com>
2026-01-17 13:50:29 -0300

    Classe com os dados dos funcionários

---
+++ Gerenciamento_funcionarios/src/classes/cadastro_funcionario.java
@@ -0,0 +1,48 @@
+
+package classes;
+
+import java.time.LocalDate;
+
+
+public class cadastro_funcionario {
+    
+    private String nome;
+    private String email;
+    private LocalDate entrada;
+    private LocalDate saida;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getEntrada() {
+        return entrada;
+    }
+
+    public void setEntrada(LocalDate entrada) {
+        this.entrada = entrada;
+    }
+
+    public LocalDate getSaida() {
+        return saida;
+    }
+
+    public void setSaida(LocalDate saida) {
+        this.saida = saida;
+    }
+    
+    
+    
+}
