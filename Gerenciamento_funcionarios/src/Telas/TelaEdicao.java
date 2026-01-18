a60b7a962e1af60e23765727158c45e289289b42
Tamiris <tamiriskdesouza@gmail.com>
2026-01-18 14:04:23 -0300

    Update TelaEdicao.java

--- Gerenciamento_funcionarios/src/Telas/TelaEdicao.java
+++ Gerenciamento_funcionarios/src/Telas/TelaEdicao.java
@@ -1,6 +1,15 @@
 
 package Telas;
 
+import classes.lista_funcionario;
+import classes.cadastro_funcionario;
+import java.text.ParseException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class TelaEdicao extends javax.swing.JFrame {
     
     private TelaInicial tela_inicial;
@@ -24,8 +33,8 @@ public class TelaEdicao extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
+        sim_b = new javax.swing.JRadioButton();
+        nao_b = new javax.swing.JRadioButton();
         botao_salvar = new javax.swing.JButton();
         nome_text = new javax.swing.JTextField();
         email_text = new javax.swing.JTextField();
@@ -44,19 +53,19 @@ public class TelaEdicao extends javax.swing.JFrame {
         jLabel3.setText("Email:");
 
         jLabel4.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel4.setText("Entrada:");
+        jLabel4.setText("Hora de entrada:");
 
         jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel5.setText("Saída:");
+        jLabel5.setText("Hora de saída:");
 
         jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel6.setText("Trabalhou hoje?");
 
-        jRadioButton1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jRadioButton1.setText("Sim");
+        sim_b.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        sim_b.setText("Sim");
 
-        jRadioButton2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jRadioButton2.setText("Não");
+        nao_b.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        nao_b.setText("Não");
 
         botao_salvar.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         botao_salvar.setText("Salvar");
@@ -79,37 +88,42 @@ public class TelaEdicao extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(23, 23, 23)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel1)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel4)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(entrada_text, javax.swing.GroupLayout.DEFAULT_SIZE, 261, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel5)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(saida_text))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel6)
                         .addGap(18, 18, 18)
-                        .addComponent(jRadioButton1)
+                        .addComponent(sim_b)
                         .addGap(18, 18, 18)
-                        .addComponent(jRadioButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(email_text, javax.swing.GroupLayout.DEFAULT_SIZE, 274, Short.MAX_VALUE)
-                            .addComponent(nome_text))))
-                .addContainerGap(42, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(botao_salvar)
-                .addGap(31, 31, 31))
+                        .addComponent(nao_b, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(nome_text)
+                                    .addComponent(email_text)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addComponent(jLabel5)
+                                        .addGap(0, 0, Short.MAX_VALUE))
+                                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                        .addGap(0, 0, Short.MAX_VALUE)
+                                        .addComponent(jLabel4)))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                    .addComponent(entrada_text, javax.swing.GroupLayout.DEFAULT_SIZE, 216, Short.MAX_VALUE)
+                                    .addComponent(saida_text)))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(0, 0, Short.MAX_VALUE)
+                                .addComponent(botao_salvar)))
+                        .addGap(35, 35, 35))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -135,11 +149,11 @@ public class TelaEdicao extends javax.swing.JFrame {
                 .addGap(25, 25, 25)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel6)
-                    .addComponent(jRadioButton1)
-                    .addComponent(jRadioButton2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
+                    .addComponent(sim_b)
+                    .addComponent(nao_b))
+                .addGap(18, 18, 18)
                 .addComponent(botao_salvar)
-                .addGap(35, 35, 35))
+                .addContainerGap(34, Short.MAX_VALUE))
         );
 
         pack();
@@ -147,12 +161,48 @@ public class TelaEdicao extends javax.swing.JFrame {
 
     private void botao_salvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botao_salvarActionPerformed
         
+    lista_funcionario listaFuncionario = new lista_funcionario();
+    cadastro_funcionario cadastro = new cadastro_funcionario();
+    TelaInicial tela_inicial = new TelaInicial();
+    
+
+    String falseTrue = "";
+        if (sim_b.isSelected()){
+           falseTrue = "Sim"; 
+           cadastro.setTrabalhou_hoje(falseTrue);
+
+        } else {
+            falseTrue = "Não";
+            cadastro.setTrabalhou_hoje(falseTrue);
+        }
         
+        try {
+           
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
+            LocalTime entrada = LocalTime.parse(entrada_text.getText(), formatter);
+            LocalTime saida = LocalTime.parse(saida_text.getText(), formatter);
+            
+            cadastro.setNome(nome_text.getText());
+            cadastro.setEmail(email_text.getText());
+            cadastro.setEntrada(entrada);
+            cadastro.setSaida(saida);
+            
+            listaFuncionario.setDadosFuncionario(cadastro);
+
+            if (tela_inicial != null) {
+                tela_inicial.atualizarTabela();
+            }
+
+            tela_inicial.atualizarTabela();
+            dispose();
+        
+        } catch (Exception ex) {
+            Logger.getLogger(TelaEdicao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    
     }//GEN-LAST:event_botao_salvarActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -195,9 +245,9 @@ public class TelaEdicao extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
+    private javax.swing.JRadioButton nao_b;
     private javax.swing.JTextField nome_text;
     private javax.swing.JTextField saida_text;
+    private javax.swing.JRadioButton sim_b;
     // End of variables declaration//GEN-END:variables
 }
